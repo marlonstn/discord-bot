95cf958f14eb737ab14f254dbf4bc1d2b7a8b31f
marlonstn <marlonstnlima@gmail.com>
2025-02-19 23:56:58 -0300

    Criei a pasta events com os arquivos ready.js, interactionCreate.js e, modifiquei o arquivo index.js

--- index.js
+++ index.js
@@ -38,6 +38,19 @@ for (const folder of commandFolders) {
 	}
 }
 
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
     const command = interaction.client.commands.get(interaction.commandName);
